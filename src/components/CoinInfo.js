5170fa9643fb530836e54d589265343552b99a8a
Victor <career.victorobije@gmail.com>
2024-02-09 14:06:39 +0100

    coinInfo

--- src/components/CoinInfo.js
+++ src/components/CoinInfo.js
@@ -1,22 +1,34 @@
-import { Container, Typography,  ThemeProvider } from "@mui/material";
-import { makeStyles } from "@mui/styles";
+import { Container, Typography, ThemeProvider } from "@mui/material";
+import { styled } from "@mui/system";
 import React, { useEffect, useState } from "react";
+import { Line } from "react-chartjs-2";
 import { useParams } from "react-router-dom";
 import { ContextState } from "../Context";
 
-const useStyles = makeStyles(() => ({}));
+const InfoContainer = styled("div")(({ theme }) => ({
+  width: "75%",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  marginTop: 25,
+  padding: 40,
+  [theme.breakpoints.down("md")]: {
+    width: "100%",
+    marginTop: 0,
+    padding: 20,
+    paddingTop: 0,
+  },
+}));
 
 const CoinInfo = () => {
-    const {id} = useParams()
-    const [coin, setCoin] = useState()
+  const { id } = useParams();
+  const [coin, setCoin] = useState();
 
-    const{currency, symbol} = ContextState();
-
+  const { currency, symbol } = ContextState();
 
   const classes = useStyles();
   return <div> hello</div>;
 };
 
 export default CoinInfo;
-
-
